f7f1860b9adf2f7b9838e02df0d7e9af80e64af9
PythonEESD2021 <ctran@eesd.org>
2021-01-26 00:50:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+'''
+01/14/2021
+
+Review
+
+integer - a whole number
+floating point - any numbe with decimal
+string - anything inside quotations
+boolean - True or False
+
+Formula creating a variable
+
+variableName = Data/Value
+
+
+Concatenation
+Appends two or more strings as a larger string.
+
+To concatenate, we use + between strings
+
+Typecast
+Change an entity from one data type to a different data type
+
+str(DATA) - To change to string
+int(DATA) - To change to integer
+float(DATA) - To change to floating point
+bool(DATA) - To change to boolean
+
+chr() - ASCII 
+'''
+
+print ("hello" + " " + "world")
+
+age = 5
+print ("I am " + str(age) + " years old")
+
+boolean = "True"
+print (bool(boolean))
